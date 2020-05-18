83c2b05b13beed391b0474d83682805195a2ce07
Tong Yin <tongyin002@gmail.com>
2020-05-18 16:38:08 -0700

    page sitemap done

---
+++ src/pages/sitemap.js
@@ -0,0 +1,99 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import SEO from "../components/seo";
+import LayoutWhite from "../components/LayoutWriting";
+
+export default function NowPage() {
+  return (
+    <LayoutWhite>
+      <SEO title="Sitemap" description="tyin sitemap" />
+      <section className="mx-2/25 flex-1 md:m-px50">
+        <div className="hyphens-auto md:max-w-px550 md:hyphens-none">
+          <h1 className="mt-px20 mb-px30 text-4p5r tracking-npx6 leading-1em text-black font-helvetica md:text-5p75r">
+            Sitemap
+          </h1>
+          <div id="sitemap" className="md:grid md:grid-cols-3">
+            <div className="mb-px20">
+              <h2 className="font-helvetica font-semibold mb-px10 uppercase text-p7r">
+                Core
+              </h2>
+              <ul>
+                <li className="mb-px3">
+                  <Link
+                    to="/about"
+                    className="border-b border-black hover:border-white"
+                  >
+                    About
+                  </Link>
+                </li>
+                <li className="mb-px3">
+                  <Link
+                    to="/"
+                    className="border-b border-black hover:border-white"
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="mb-px3">
+                  <Link
+                    to="/now"
+                    className="border-b border-black hover:border-white"
+                  >
+                    Now
+                  </Link>
+                </li>
+                <li className="mb-px3">
+                  <Link
+                    to="/sitemap"
+                    className="border-b border-black hover:border-white"
+                  >
+                    Sitemap
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-px20">
+              <h2 className="font-helvetica font-semibold mb-px10 uppercase text-p7r">
+                Writing
+              </h2>
+              <ul>
+                <li className="mb-px3">
+                  <Link
+                    to="/dev-posts"
+                    className="border-b border-black hover:border-white"
+                  >
+                    Dev-Posts
+                  </Link>
+                </li>
+                <li className="mb-px3">
+                  <Link
+                    to="/fragments"
+                    className="border-b border-black hover:border-white"
+                  >
+                    Fragments
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-px20">
+              <h2 className="font-helvetica font-semibold mb-px10 uppercase text-p7r">
+                Other
+              </h2>
+              <ul>
+                <li className="mb-px3">
+                  <Link
+                    to="/photos"
+                    className="border-b border-black hover:border-white"
+                  >
+                    Photography
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </LayoutWhite>
+  );
+}
